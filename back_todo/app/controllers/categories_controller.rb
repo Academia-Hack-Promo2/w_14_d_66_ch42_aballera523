b9704152a640b77e8e26d9cbb55850bee8b17b46
maria <mariiromero17@gmail.com>
2015-05-19 15:29:04 -0430

    listar las categorias

--- back_todo/app/controllers/categories_controller.rb
+++ back_todo/app/controllers/categories_controller.rb
@@ -1,2 +1,28 @@
 class CategoriesController < ApplicationController
+  def create 
+    categories = Category.new(permit)
+    if categories.valid?
+      categories.save
+      render json: categories
+    else
+      render json: categories.errors
+    end
+  end
+    
+
+
+	def list
+		categories = Category.all 
+		if categories == nil
+			render json:  {"error" => "No hay categorias que listar"}
+    else
+      render json: categories
+    end   
+	end
+
+  def permit
+    params.permit(:name)
+  end
 end
+
+
--- back_todo/config/routes.rb
+++ back_todo/config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   delete 'tasks/:id' => 'tasks#destroy'
   #resources :tasks
 
+  post 'categories' => 'categories#create'
+  get 'categories' => 'categories#list'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
