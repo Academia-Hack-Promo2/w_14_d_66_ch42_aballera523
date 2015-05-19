70c08158fbf4f173871bb07a509fc44c91130a3a
greyes193 <gen.reyest@gmail.com>
2015-05-19 14:42:55 -0430

    Modificada la indentacion para que sea por TABULACION

--- back_todo/app/controllers/tasks_controller.rb
+++ back_todo/app/controllers/tasks_controller.rb
@@ -1,8 +1,8 @@
 class TasksController < ApplicationController
-  def index
-    task = Task.all 
-    render json: task
-  end
+	def index
+		task = Task.all 
+		render json: task
+	end
 	def new
 		task = Task.new(permit)
 		if task.valid?
@@ -12,15 +12,15 @@ class TasksController < ApplicationController
 			render json: task.errors.messages			
 		end
 	end
-  def update
-    valid = Task.exists?(params[:id].to_i)
-    if valid
-      task = Task.update(params[:id].to_i, permit)
-      render json: task
-    else
-      render json: task.errors.messages
-    end
-  end
+	def update
+		valid = Task.exists?(params[:id].to_i)
+		if valid
+			task = Task.update(params[:id].to_i, permit)
+			render json: task
+		else
+			render json: task.errors.messages
+		end
+	end
 	private
 		def permit
 			params.permit(:title, :status, :date, :priority, :category_id)
