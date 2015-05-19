76d20da89e83571ccfe57e50d759055f13578e0c
marco <maldonmarco@gmail.com>
2015-05-19 11:35:56 -0430

    Crear las Clases correspondientes a Tareas en el modelo de datos = modificado

--- back_todo/db/migrate/20150519154558_create_tasks.rb
+++ back_todo/db/migrate/20150519154558_create_tasks.rb
@@ -1,9 +1,9 @@
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
-      t.string :title, null: false, limit:150
-      t.string :status, null: false, limit:12
-      t.string :priority, null: false, limit:12
+      t.string :title, null: false, limit: 150
+      t.string :status, null: false, limit: 12
+      t.string :priority, null: false, limit: 12
       t.date :date
       t.references :category, index: true, foreign_key: true
 
