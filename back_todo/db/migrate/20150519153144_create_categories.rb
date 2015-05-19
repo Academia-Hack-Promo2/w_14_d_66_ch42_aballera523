9a20c0f76ceef1927f1e0b6bdf030cfc588eb47c
Alex Ballera <alexballera@gmail.com>
2015-05-19 11:08:01 -0430

    Crear Clases a Categorias En El Modelo De Datos

---
+++ back_todo/db/migrate/20150519153144_create_categories.rb
@@ -0,0 +1,9 @@
+class CreateCategories < ActiveRecord::Migration
+  def change
+    create_table :categories do |t|
+      t.string :name null: false, limit: 20
+
+      t.timestamps null: false
+    end
+  end
+end
