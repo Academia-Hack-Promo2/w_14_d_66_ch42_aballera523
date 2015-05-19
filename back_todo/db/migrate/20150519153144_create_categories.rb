c6fbc790e11e2adb5ae18357f42b384e1fd7a139
Alex Ballera <alexballera@gmail.com>
2015-05-19 11:38:16 -0430

    Correcion del modelo Categorias

--- back_todo/db/migrate/20150519153144_create_categories.rb
+++ back_todo/db/migrate/20150519153144_create_categories.rb
@@ -1,7 +1,7 @@
 class CreateCategories < ActiveRecord::Migration
   def change
     create_table :categories do |t|
-      t.string :name null: false, limit: 20
+      t.string :name, null: false, limit: 20
 
       t.timestamps null: false
     end
