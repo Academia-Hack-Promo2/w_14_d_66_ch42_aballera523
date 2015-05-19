a61abd37b6742c314b820f0b47d275b8c335bbaf
marco <maldonmarco@gmail.com>
2015-05-19 15:06:05 -0430

    modificacion status

--- back_todo/app/models/task.rb
+++ back_todo/app/models/task.rb
@@ -2,9 +2,9 @@ class Task < ActiveRecord::Base
   belongs_to :category
 
 	validates :title, presence: true, length: { maximum: 255 }
-	validates :status, presence: true, inclusion: { in: %w(low  high  medium),
+	validates :status, presence: true, inclusion: { in: %w(done undone),
     message: "%{value} option is not valid" }
-	validates :priority, presence: true, inclusion: { in: %w(done undone),
+	validates :priority, presence: true, inclusion: { in: %w(low  high  medium),
     message: "%{value} option is not valid" }
 	validates :date, presence: true
 
