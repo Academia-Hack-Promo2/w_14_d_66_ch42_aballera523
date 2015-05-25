51e036085a973351248a734479c94228e93e898f
Alex Ballera <alexballera@gmail.com>
2015-05-25 20:27:54 +0000

    Arreglado Las Tareas Muestran Categoria

--- front_todo/app/scripts/task.js
+++ front_todo/app/scripts/task.js
@@ -1,6 +1,7 @@
 var Task = function(container, data){
   url = 'http://localhost:3000/tasks'
   this.container = container;
+  console.log(data);
 
   if(container) {    
     this.id = container.data('task');
@@ -15,28 +16,28 @@ var Task = function(container, data){
 }
 
 Task.prototype.init = function(data) {
-  this.id = data.id;
-  this.title = data.title;
-  this.status = data.status;
-  this.priority = data.priority;
-  this.date = data.date;
-  this.category_id = data.category_id;
+  id = data.id;
+  title = data.title;
+  status = data.status;
+  priority = data.priority;
+  date = data.date;
+  category = data.category.name;
 };
 
 Task.prototype.draw = function() {
 
-  builder = $("<div/>",{id:'task_'+this.id,class:"col-xs-8 col-sm-5 col-md-3"}).append(
+  builder = $("<div/>",{id:'task_'+id,class:"col-xs-8 col-sm-5 col-md-3"}).append(
     $("<div/>",{class:"row task-gradient"}).append(
       $("<div/>",{class:"row task-header"}).append(
-        $("<div/>",{class:"col-md-3 text-center task-priority date-priority-margin"}).html(this.priority),
-        $("<div/>",{class:"col-md-9 text-center task-date date-priority-margin"}).html(this.date)),
+        $("<div/>",{class:"col-md-3 task-priority"}).html(priority),
+        $("<div/>",{class:"col-md-9 task-date"}).html(date)),
 
-      $("<div/>",{class:"row text-center task-content"}).html(this.title),
+      $("<div/>",{class:"row task-title"}).html(title),
 
       $("<div/>",{class:"row image"}).append(
-        $("<img/>",{class:"activator", src:"images/task.jpg"})),
+        $("<img/>",{class:"probando", src:"images/task.jpg"})),
 
-      $("<div/>",{class:"row text-center task-category"}).html(this.category_id)),
+      $("<div/>",{class:"row task-category"}).html(category)),
 
     $("<div/>",{class:"row task-footer"}).append(
       $("<div/>",{class:"col-md-6 task-done"}).html('Done'),
