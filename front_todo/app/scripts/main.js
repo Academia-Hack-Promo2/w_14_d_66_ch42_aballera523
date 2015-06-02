79fc7c4043443130ff5c2264089ae2114d6091cf
Hector <hector12j@gmail.com>
2015-06-02 13:12:52 -0430

    listo el todo

--- front_todo/app/scripts/main.js
+++ front_todo/app/scripts/main.js
@@ -2,10 +2,17 @@ $(function(){
 
   var $tasksSection = $('.tasksSection');
   var tasks = new Tasks($tasksSection);
-
+  
   var $categoriesSection = $('.categoriesSection');
   var categories = new Categories($categoriesSection);
 
+  $(function(){
+  	console.log("aqui");
+  	var toogles = $('.task-done');
+  	for (var i = 0; i < toogles.length; i++) {
+  		
+  	};
+  }());
 //provando
 
 	var printNewCategory = function(data){
@@ -35,16 +42,27 @@ $(function(){
 			}
 		});
 	}
-	var	editTask = function(id, edit, query){
+	var	editTask = function(id, edit, query,taskId){
 		$.ajax({
 		 		url:'http://localhost:3000/tasks/'+id,
 		 		type:query,
 		 		data: edit,
 		 		success: function(data){	
-		 			console.log(data.date);
+		 			
 		 			$('#date_'+id).html(data.date); 
 		 			$('#title_'+id).html(data.title);
-		 			$('#category_'+id).html(data.category.name);			
+		 			$('#category_'+id).html(data.category.name);	
+		 			console.log(data.status);
+		 			$('#'+taskId).attr('id',data.status+'_status'+'_'+data.id)
+		 			//id:this.status+'_status'+'_'+this.id
+		 			if (data.status=="done") {
+		 				$('#status_'+id).children('div').hasClass('toogle btn ios btn-succes off');	
+		 			}
+		 			else{
+		 				$('#status_'+id).children('div').hasClass('toogle btn ios btn-danger');	
+		 			}
+		 			
+
 		 		}
 		 });
 	}
@@ -108,13 +126,29 @@ $(function(){
 			editTask(id, data, "put");
 			$('#myModal').modal('toggle');
 			
-			console.log(id);
+			
 			 
 		});
 
 	});
 	$(document).on("click",".task-left",function(){
-		console.log('aqui voy '+this.id);
+		id = this.id[this.id.length-2]+this.id[this.id.length-1];
+		var taskStatus = this.id.split("_",1);
+		console.log(taskStatus);
+		if (taskStatus=='done') {
+			taskStatus= 0
+		}
+		else {
+			taskStatus = 1	
+		}
+		data = {
+			title:$('#title_'+id).html(),
+			status:taskStatus,
+			priority:0,
+			date:$('#date_'+id).html(),
+			category_id:$('#category_'+id).data('id')
+		}
+		editTask(id,data,'put',this.id);
 	});
 
 		$(document).on("click",'.delete-category',function(){
